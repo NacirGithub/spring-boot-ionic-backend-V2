f406de72daa1bdabcccf1d23c4633568229b8f42
NacirGithub <nacibraimo@gmail.com>
2018-08-29 04:12:32 +0200

    Apagando uma categoria com DELETE

--- src/main/java/com/devprime/cursomc/services/CategoriaService.java
+++ src/main/java/com/devprime/cursomc/services/CategoriaService.java
@@ -3,6 +3,7 @@ package com.devprime.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.devprime.cursomc.domain.Categoria;
@@ -12,25 +13,34 @@ import javassist.tools.rmi.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
-	private CategoriaRepository repo; 
-	
+	private CategoriaRepository repo;
+
 	public Categoria find(Integer id) throws ObjectNotFoundException {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objecto nao encontrado! id: " + id + ",  Tipo: " + Categoria.class.getName()));
-	} 
-	
+	}
 
 	public Categoria save(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) throws ObjectNotFoundException {
 		find(obj.getId());
 		return repo.save(obj);
-		
+
+	}
+
+	public void delete(Integer id) throws ObjectNotFoundException {
+		find(id);
+		try {
+			repo.deleteById(id);
+
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException("Nao foi possivel apagar a categoria, porque possui produtos");
+		}
 	}
 }
--- src/main/java/com/devprime/cursomc/services/exceptions/ObjectNotFoundException.java
+++ src/main/java/com/devprime/cursomc/services/exceptions/ObjectNotFoundException.java
@@ -2,9 +2,6 @@ package com.devprime.cursomc.services.exceptions;
 
 public class ObjectNotFoundException extends RuntimeException{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	public ObjectNotFoundException(String msg) {
