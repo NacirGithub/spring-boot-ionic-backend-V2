6c99f6a746896318f40fcc8367e4dc7c7659e8e5
NacirGithub <nacibraimo@gmail.com>
2018-09-06 15:44:21 +0200

    Email nao repetido na insercao de cliente

--- src/main/java/com/devprime/cursomc/services/validation/ClienteCreateNomeValidator.java
+++ src/main/java/com/devprime/cursomc/services/validation/ClienteCreateNomeValidator.java
@@ -2,15 +2,23 @@ package com.devprime.cursomc.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.devprime.cursomc.domain.Cliente;
 import com.devprime.cursomc.domain.enums.TipoCliente;
 import com.devprime.cursomc.dto.ClienteNewDTO;
+import com.devprime.cursomc.repositories.ClienteRepository;
 import com.devprime.cursomc.resources.exceptions.FieldMessage;
 import com.devprime.cursomc.services.validation.utils.BR;
 
 public class ClienteCreateNomeValidator implements ConstraintValidator<ClienteCreate, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
 	
 	@Override
 	public void initialize(ClienteCreate ann) {
@@ -28,6 +36,12 @@ public class ClienteCreateNomeValidator implements ConstraintValidator<ClienteCr
 		if (objDTO.getTipoCliente().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDTO.getCodigoPostal())) {
 			list.add(new FieldMessage("codigoPostal", "Codigo Postal 2 invalido "));
 		}
+		
+		Cliente aux = repo.findByEmail(objDTO.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email jå existente"));
+		}
+		
 		
 		// inclua os testes aqui, inserindo erros na lista
 		for (FieldMessage e : list) {
