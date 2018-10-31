84542f25d4a5ced2c4059169a113a94307e2472a
NacirGithub <nacibraimo@gmail.com>
2018-10-31 15:32:22 +0200

    EndPoint para buscar estatus e cidades

--- src/main/java/com/devprime/cursomc/repositories/EstadoRepository.java
+++ src/main/java/com/devprime/cursomc/repositories/EstadoRepository.java
@@ -1,9 +1,14 @@
 package com.devprime.cursomc.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devprime.cursomc.domain.Estado;
 
 public interface EstadoRepository extends JpaRepository<Estado, Integer> {
-
-}
+	
+	@Transactional(readOnly=true)
+	public List<Estado> findAllByOrderByNome(); 
+} 
---
+++ src/main/java/com/devprime/cursomc/services/EstadoService.java
@@ -0,0 +1,20 @@
+package com.devprime.cursomc.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devprime.cursomc.domain.Estado;
+import com.devprime.cursomc.repositories.EstadoRepository;
+
+@Service
+public class EstadoService {
+
+	@Autowired
+	private EstadoRepository repo;
+	
+	public List<Estado> findAll(){
+		return repo.findAllByOrderByNome();
+	}
+}
